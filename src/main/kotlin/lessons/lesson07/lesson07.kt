99b7874c71da6ca0867c2b38274e702f9d189b77
kva0904 <vkomissarovhd@gmail.com>
2025-09-26 19:43:49 +0500

    7_1

---
+++ src/main/kotlin/lessons/lesson07/lesson07.kt
@@ -0,0 +1,51 @@
+package lessons.lesson07
+
+fun main() {
+
+//    for (i in 1..10) {
+//        println(i)
+//    }
+//
+//    for (i in 10 downTo 1) {
+//        println(i)
+//    }
+//
+//    for (i in 1..10 step 2) {
+//        println(i)
+//    }
+//
+//    for (i in 1 until 10) {
+//        println(i)
+//    }
+//
+//    var range: Array<1,2,3>
+//    for (i in range) {
+//        println(i)
+//    }
+//
+//    var counter = 0
+//    while (counter++ <= 10) {
+//        println(counter)
+//    }
+//
+//    var counter = 0
+//    do {
+//        println(counter)
+//    } while (counter++ < 10)
+//
+//    for (i in 1 until 10) {
+//        if (i == 3) break
+//        println(i)
+//    }
+//
+//    for (i in 1 until 10) {
+//        if (i == 3) continue
+//        println(i)
+//    }
+
+    var lenght: Int = 34
+    for (i in 1 until lenght) {
+
+    }
+
+}
