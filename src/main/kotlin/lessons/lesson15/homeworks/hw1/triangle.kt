fd5e126b233114ee873047a92332370cdb0b06bc
kva0904 <vkomissarovhd@gmail.com>
2025-10-27 19:42:30 +0500

    15_2

--- src/main/kotlin/lessons/lesson15/homeworks/hw1/triangle.kt
+++ src/main/kotlin/lessons/lesson15/homeworks/hw1/triangle.kt
@@ -6,8 +6,5 @@ class triangle(
     val sideC: Double
 ) : polygon("Треугольник", 3) {
 
-    override fun area(): Double {
-        val p = perimeter() / 2
-        return Math.sqrt(p * (p - sideA) * (p - sideB) * (p - sideC))
     }
 }
